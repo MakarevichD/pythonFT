33a1040faf23fb71fc98e885570e069e7d5593ea
dmakarevich-ms <d.makarevich@melsoft-games.com>
2022-03-12 12:22:05 +0400

    delete filee

--- contacts.py
+++
@@ -1,8 +0,0 @@
-class Contact:
-
-    def __init__(self, contact_name, contact_surname, work_num, mobile_num):
-        self.contact_name = contact_name
-        self.contact_surname = contact_surname
-        self.work_num = work_num
-        self.mobile_num = mobile_num
-
