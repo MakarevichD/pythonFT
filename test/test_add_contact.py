e5e265ec6cf1be8ea55eaf75c619cc94080dbf6f
dmakarevich-ms <d.makarevich@melsoft-games.com>
2022-03-12 12:24:30 +0400

    changed class import

---
+++ test/test_add_contact.py
@@ -0,0 +1,82 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from selenium import webdriver
+import unittest
+from contact import Contact
+
+
+
+class AddContactsTest(unittest.TestCase):
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--incognito")
+        self.wd = webdriver.Chrome(options=options)
+        self.wd.implicitly_wait(30)
+
+    def open_homepage(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_homepage()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def add_new_contact(self, contacts):
+        wd = self.wd
+        self.open_homepage()
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contacts.contact_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contacts.contact_surname)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contacts.mobile_num)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contacts.work_num)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_homepage()
+
+    def return_to_homepage(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_new_contact_adding(self):
+        self.login( username="admin", password="secret")
+        self.add_new_contact( Contact(contact_name="Gena", contact_surname="Babkov", work_num="432 4432", mobile_num="34 99800 "))
+        self.logout()
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
