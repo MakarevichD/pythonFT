21761920c9eb1b405f27a76af68208edb72b88b1
dmakarevich-ms <d.makarevich@melsoft-games.com>
2022-03-16 00:42:23 +0400

    added fixture folder

---
+++ fixture/contact.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+
+
+class ContactWriter:
+
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--incognito")
+        self.wd = webdriver.Chrome(options=options)
+        self.wd.implicitly_wait(30)
+
+    def open_homepage(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_homepage()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def add_new_contact(self, contacts):
+        wd = self.wd
+        self.open_homepage()
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contacts.contact_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contacts.contact_surname)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contacts.mobile_num)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contacts.work_num)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_homepage()
+
+    def return_to_homepage(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def destroy(self):
+        self.wd.quit()
