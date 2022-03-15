315284f1f5ee8e494353e61123cf0373f1811ade
dmakarevich-ms <d.makarevich@melsoft-games.com>
2022-03-16 00:43:51 +0400

    added fixture folder

---
+++ fixture/application.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+
+class Application:
+
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("--incognito")
+        self.wd = webdriver.Chrome(options=options)
+        self.wd.implicitly_wait(30)
+
+    def open_homepage(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_homepage()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def group_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+        wd.implicitly_wait(120)
+
+    def group_creation(self, group):
+        wd = self.wd
+        self.group_page()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+        self.return_to_group_page()
+
+    def return_to_group_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def destroy(self):
+        self.wd.quit()
+
+
+
+
