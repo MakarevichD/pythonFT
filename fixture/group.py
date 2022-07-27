787b0c4d1d9dcd453e291e89830a749194d91507
dmakarevich-ms <d.makarevich@melsoft-games.com>
2022-07-27 18:29:27 +0300

    added additional methods, new test for group modificate

--- fixture/group.py
+++ fixture/group.py
@@ -8,43 +8,44 @@ class GroupHelper:
         wd.find_element_by_xpath("//a[@href = 'group.php']").click()
         wd.implicitly_wait(30)
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group.footer", group.footer)
+
     def creation(self, group):
         wd = self.app.wd
         self.group_page()
         wd.find_element_by_xpath("//input[@value = 'New group']").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
     def delete_first_group(self):
         wd = self.app.wd
         self.group_page()
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_xpath("//input[@value ='Delete group(s)']").click()
         self.return_to_group_page()
 
-    def modify_group(self, group):
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.group_page()
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_xpath("//input[@value ='Edit group']").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(new_group_data)
         wd.find_element_by_xpath("//input[@value ='Update']").click()
         self.return_to_group_page()
 
--- test/test_group_modification.py
+++ test/test_group_modification.py
@@ -1,7 +1,12 @@
 from model.group import Group
 
 
-def test_group_modificate(app):
+def test_group_modificate_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_group(Group(name="lapki",header="pushistie",footer="kotikov"))
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_group_modificate_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
