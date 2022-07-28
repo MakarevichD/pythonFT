31a458874309e3016ee29acd9d62b9cf8e529638
dmakarevich-ms <d.makarevich@melsoft-games.com>
2022-07-28 16:30:20 +0300

    fixed test failure

--- conftest.py
+++ conftest.py
@@ -1,19 +1,17 @@
 import pytest
 from fixture.application import Application
 
-
 fixture = None
 
 @pytest.fixture
-def app(request):
+def app():
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+    fixture.session.login(username="admin", password="secret")
     return fixture
 
 
@@ -22,5 +20,6 @@ def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
+
     request.addfinalizer(fin)
     return fixture
--- fixture/application.py
+++ fixture/application.py
@@ -14,12 +14,6 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
-    def is_valid(self):
-        try:
-            self.wd.current_url
-            return True
-        except:
-            return False
 
     def open_homepage(self):
         wd = self.wd
@@ -27,3 +21,10 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
+
+    def is_valid(self):
+        try:
+            self.wd.current_url()
+            return True
+        except:
+            return False
