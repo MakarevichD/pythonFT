8528714e13b9d8cab9edf0f433130b9a6ca7cfb3
dmakarevich-ms <d.makarevich@melsoft-games.com>
2022-11-22 00:30:40 +0200

    fixed overriding contact_edit fields by old values

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 def test_new_contact_adding(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(contact_name="Gena", contact_surname="Babkov", work_num="432 4432", mobile_num="34 99800 ")
+    contact = Contact(contact_name="Gena", contact_surname="Babkov", work_num="432 4432", mobile_num="34 99800 ", second_num="2313", home_num="23232")
     app.contact.add_new_contact(contact)
     assert len(old_contacts) + 1 == app.contact.contacts_count()
     new_contacts = app.contact.get_contact_list()
