ff6e4fff7b445e54a23104a643809b7d6d6c7d8c
dmakarevich-ms <d.makarevich@melsoft-games.com>
2022-12-07 03:44:03 +0200

    objects relation

--- fixture/orm.py
+++ fixture/orm.py
@@ -6,7 +6,6 @@ from pymysql.converters import decoders
 
 
 class ORMFixture:
-
     db = Database()
 
     class ORMGroup(db.Entity):
@@ -15,6 +14,7 @@ class ORMFixture:
         name = Optional(str, column="group_name")
         header = Optional(str, column="group_header")
         footer = Optional(str, column="group_footer")
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups",lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = "addressbook"
@@ -22,27 +22,42 @@ class ORMFixture:
         contact_name = Optional(str, column="firstname")
         contact_surname = Optional(str, column="lastname")
         deprecated = Optional(datetime, column="deprecated")
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts",lazy=True)
 
-    def __init__(self,host, name, user, password):
-        self.db.bind('mysql', host=host, database=name, user=user, password=password)#, conv=decoders) #with this converter i've got str instead of datetime
+    def __init__(self, host, name, user, password):
+        self.db.bind('mysql', host=host, database=name, user=user,
+                     password=password)#, conv=decoders)  # with this converter i've got str instead of datetime
         self.db.generate_mapping()
         sql_debug(True)
 
-    def convert_groups_to_model(self,groups):
+    def convert_groups_to_model(self, groups):
         def convert(group):
             return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
+
         return list(map(convert, groups))
 
-    def convert_contacts_to_model(self,contacts):
+    def convert_contacts_to_model(self, contacts):
         def convert(contact):
-            return Contact(id=str(contact.id), contact_name=contact.contact_name, contact_surname=contact.contact_surname)
+            return Contact(id=str(contact.id), contact_name=contact.contact_name,
+                           contact_surname=contact.contact_surname)
+
         return list(map(convert, contacts))
 
     @db_session
+    def get_contacts_in_groups(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(orm_group.contacts)
+
+    @db_session
+    def get_contacts_not_in_groups(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
+
+    @db_session
     def get_group_list(self):
-        return self.convert_groups_to_model(select( g for g in ORMFixture.ORMGroup))
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
 
     @db_session
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
-
